0652ca8246a4a392a5367f3add2ccdae58f91079
Lem Euro <mr.lemeuro@gmail.com>
2023-03-12 16:30:07 +0000

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Lem Euro on 12/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
